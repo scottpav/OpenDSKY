638fe2ea191560af947af6b089eb8d03be63117a
Scott Pavlovec <pavsalarm@gmail.com>
2018-06-27 15:53:40 -0500

    Update OpenDsky.ino

--- OpenDsky.ino
+++ OpenDsky.ino
@@ -82,7 +82,7 @@ void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(1);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   rtc.begin();    
   Serial.begin(9600);
